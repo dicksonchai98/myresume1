fd963be9d2ac00f10605f32079cc4960f7354f6f
dicksonchai98 <102925011+dicksonchai98@users.noreply.github.com>
2024-04-04 17:16:40 +0800

    Add files via upload

---
+++ myresume.js
@@ -0,0 +1,8 @@
+function openTab(tabName) {
+  var i;
+  var tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
